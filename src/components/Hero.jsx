d8068dda8fccc6bcb0ec3304923da47a270dbe7c
Raji Sofia <mrsbeast@Rajis-MacBook-Pro.local>
2025-09-08 13:09:20 +0100

    commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
+import arrow from "../assets/images/Arrow.png"
+import myimage from "../assets/images/me.png"
 
 export default function Hero() {
     return (
         <div className='flex gap-[40px] items-center pl-[15em] pt-[100px]'>
             <div className='relative'>
-                <img src="src/assets/images/Arrow.png" alt="arrow" className='absolute inset-y-0 -top-[80px] left-[100px] animate-bounce' />
+                <img src={arrow} alt="arrow" className='absolute inset-y-0 -top-[80px] left-[100px] animate-bounce' />
                 <span className='text-white absolute -top-[55px] left-[210px] whitespace-nowrap'>Hello, I am <span className='text-[#7127BA]'>Sofia Raji</span> </span>
                 <div className='relative'>
                     <div className='bg-[rgba(118,60,172,1)] blur-xl drop-shadow-2xl h-[280px] w-[150px] rounded-full absolute left-[20px] shadow-2xl '></div>
-                    <img src="src/assets/images/me.png" alt="my-image" className='h-[250px] relative z-[10px] animate-bounce' />
+                    <img src={myimage} alt="my-image" className='h-[250px] relative z-[10px] animate-bounce' />
                 </div>
             </div>
             <div className='text-white'>
